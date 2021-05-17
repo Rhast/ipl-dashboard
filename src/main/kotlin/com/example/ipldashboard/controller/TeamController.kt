ba7dc063e335debb85319635c69201c853dcc090
Rhast <0rhast0@gmail.com>
2021-05-18 00:25:52 +0300

    + react app

--- src/main/kotlin/com/example/ipldashboard/controller/TeamController.kt
+++ src/main/kotlin/com/example/ipldashboard/controller/TeamController.kt
@@ -5,11 +5,13 @@ import com.example.ipldashboard.controller.dto.TeamDto
 import com.example.ipldashboard.controller.errors.TeamNotFoundException
 import com.example.ipldashboard.repository.TeamRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin("*")
 class TeamController @Autowired constructor(
     val teamRepository: TeamRepository,
     val matchDao: MatchDao
