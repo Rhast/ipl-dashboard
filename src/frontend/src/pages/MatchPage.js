81c089c2fa30ff15698c009a8b9dcf7f0e048f30
Rhast <0rhast0@gmail.com>
2021-05-18 23:25:00 +0300

    TeamPage ready + MatchPage

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -3,7 +3,7 @@ import {MatchDetailCard} from '../components/MatchDetailCard';
 import {MatchSmallCard} from '../components/MatchSmallCard';
 import {useParams} from 'react-router-dom';
 
-export const TeamPage = () => {
+export const MatchPage = () => {
     const [team, setTeam] = useState({matches: []});
     const {teamName} = useParams();
     useEffect(() => {
@@ -20,7 +20,7 @@ export const TeamPage = () => {
         return <h1>Team not found</h1>;
     }
     return (
-        <div className="TeamPage">
+        <div className="MatchPage">
             <h1>{team.teamName}</h1>
             <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
             {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName}  match={match} />)}
